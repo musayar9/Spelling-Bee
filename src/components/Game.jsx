ffe9206472d171dba0fea9e8864407640326d7a3
Musa Sayar <musasayar67@gmail.com>
2024-05-11 13:52:19 +0300

    create newValue state

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -4,18 +4,29 @@ import { useRef, useState } from "react";
 
 export default function Game({ language, dictionary }) {
   // console.log("dictionary", dictionary.letters);
-  const [words, setWords] = useState("");
-
-  let lettersShuffle = dictionary.letters.sort(() => Math.random() - 0.5);
+  const [words, setWords] = useState([]);
+  const [newValue, setNewValue] = useState("");
+  let lettersShuffle = dictionary.letters
   const ref = useRef();
   const handleClick = (e) => {
     const clickedElement = e.target.closest("li"); // Tıklanan li öğesini bulun
     const content = clickedElement.querySelector("p").textContent; // Li öğesinin içindeki p etiketinin içeriğini alın
     console.log("Tıklanan içerik:", content);
 
-    setWords({ ...words, content });
+    setWords([...words, content]);
   };
-  console.log("words", words);
+
+  function handleShowText() {
+    let newData = words.join("");
+    setNewValue(newData);
+  }
+  console.log(newValue !== null && newValue);
+  
+  const handleDelete = ()=>{
+  setNewValue("")
+  setWords([])
+  
+  }
   return (
     <>
       <div className="flex flex-col items-center mt-20">
@@ -30,6 +41,9 @@ export default function Game({ language, dictionary }) {
             </li>
           ))}
         </ul>
+
+        <button onClick={handleShowText}>show text</button>
+        <button onClick={handleDelete}>delete text</button>
       </div>
     </>
   );
