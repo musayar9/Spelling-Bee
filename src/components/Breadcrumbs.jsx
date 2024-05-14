016df2c5335cbbb30f06bd93ade2e925528fe71f
Musa Sayar <musasayar67@gmail.com>
2024-05-14 15:21:00 +0300

    edit breadcrumbs

--- src/components/Breadcrumbs.jsx
+++ src/components/Breadcrumbs.jsx
@@ -1,18 +1,26 @@
 "use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 const Breadcrumbs = () => {
+  const pathname = usePathname();
+
   return (
-    <div className="text-sm breadcrumbs max-w-xl mx-auto">
+    <div className="text-sm breadcrumbs max-w-xl mx-auto flex items-center justify-center">
       <ul>
-        <li>
+        <li className="text-2xl ">
           <Link href="/">Home</Link>
         </li>
-        <li>
-          <Link href="/turkish">Turkish</Link>
-        </li>
-        <li>
-          <Link href="/english">Turkish</Link>
-        </li>
+
+        {pathname === "/turkish" && (
+          <li className="text-2xl text-amber-400">
+            <Link href="/turkish">Turkish</Link>
+          </li>
+        )}
+        {pathname === "/english" && (
+          <li className="text-2xl text-amber-400">
+            <Link href="/english">English</Link>
+          </li>
+        )}
       </ul>
     </div>
   );
