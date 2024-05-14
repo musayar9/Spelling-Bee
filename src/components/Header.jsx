2ce29c603f54319d47be2873a3ebed372edf49f1
Musa Sayar <musasayar67@gmail.com>
2024-05-14 10:15:20 +0300

    added game rules

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,35 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <div className="flex items-center gap-4">
-      <Link href="/turkish">Turkish</Link>
-      <Link href="/english">Englisj</Link>
+    <div className="max-w-3xl mx-auto my-24">
+      <div className="flex flex-col items-center justify-center ">
+        <h1 className="text-4xl text-center">
+          Spelling <span className="text-yellow-400">Bee</span> Game
+        </h1>
+        <p className="text-xl  text-slate-500 text-center my-8 tracking-widest">
+          In this game you are given 7 letters. You are trying to find words
+          that can be formed using these seven letters. You have one minute to
+          find the words. For each word you find correctly, you will earn 15
+          seconds and points equal to the length of the word. For each wrong
+          word you lose 2 points
+        </p>
+
+        <h4 className="text-slate-500">Choose the version you want to play</h4>
+        <div className="flex gap-4 mt-4">
+          <Link
+            href="/turkish"
+            className="text-lg bg-amber-500 text-white px-6 py-2.5 rounded-full shadow-lg shadow-amber-500/50  bg-gradient-to-r from-amber-400 via-amber-500 to-amber-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-amber-300"
+          >
+            Turkish
+          </Link>
+          <Link
+            href="/english"
+            className="text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300  shadow-lg shadow-pink-500/50 font-medium rounded-full px-6 py-2.5 text-center"
+          >
+            English
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,14 @@
+import Link from "next/link";
+
 const Navbar = () => {
   return (
     <div className="  bg-emerald-500">
       <div className="max-w-6xl mx-auto p-4">
         <div className="flex items-center">
           <h4 className="text-2xl font-bold text-gray-100">
-            Spelling <span className="text-yellow-400">Bee</span> Game
+            <Link href="/">
+              Spelling <span className="text-yellow-400">Bee</span> Game
+            </Link>
           </h4>
         </div>
       </div>
