fbcaeb0e4df5c6108f588579c5792040cfaa0e8f
Musa Sayar <musasayar67@gmail.com>
2024-05-14 21:37:26 +0300

    edit favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,12 +8,12 @@ const lobster = Lobster({ weight: "400", subsets: ["latin"] });
 export const metadata = {
   title: "Spelling Bee Game",
   description: "Spelling Bee Game",
-
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      
       <body className={lobster.className}>
         <GameProvider>
           <Navbar />
